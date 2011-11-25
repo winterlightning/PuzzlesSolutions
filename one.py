6f9389fd6296a04e76cd6c9a3b22703beb2a0b2f
winterlightning <raycornellwang@gmail.com>
2011-11-25 17:43:58 -0500

    added line

--- one.py
+++ one.py
@@ -2,6 +2,7 @@
 #Find the sum of all the multiples of 3 or 5 below 1000.
 
 #solution comment: simplest solution is you can sum from 3 -> 999 and 5 -> 995, the key is adding the multiples of 3 and 5 only once
+
 #calculate the sum of the multiples of 3 and 5 up till the limit
 def calculate_multiple_sum(limit):
     return sum(range(3, limit, 3) ) + sum ( range(5, limit, 5) ) - sum (range(15, limit, 15))
